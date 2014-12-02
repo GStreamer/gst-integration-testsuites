868302c4d3a0ce69e34f8e9cceb21b4343aed833
Thibault Saunier <tsaunier@gnome.org>
2014-12-02 17:38:11 +0100

    Reactivate HLS tests running it all *locally*

---
+++ medias/defaults/bipbop/gear3/fileSequence1.ts
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/Xz/Z9/SHA256E-s648976--241238c948aad3df0871f3715763c06c3d682e46ddfd2b6e4ffa59335ebc2e9b.ts/SHA256E-s648976--241238c948aad3df0871f3715763c06c3d682e46ddfd2b6e4ffa59335ebc2e9b.ts
---
+++ medias/defaults/bipbop/gear4/fileSequence0.ts
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/KF/K9/SHA256E-s923080--7157429f8c9ec49ca56b3fc024375e127bfe99ea091ebead80d53c48756d8d0c.ts/SHA256E-s923080--7157429f8c9ec49ca56b3fc024375e127bfe99ea091ebead80d53c48756d8d0c.ts
