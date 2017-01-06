f28d5e21fe67164eb3b4a28a54a62d6fbda23e56
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-01-06 17:48:48 -0300

    Move away from git-annex

--- medias/defaults/bipbop/gear3/fileSequence1.ts
+++
@@ -1,1 +0,0 @@
-../../../../.git/annex/objects/Xz/Z9/SHA256E-s648976--241238c948aad3df0871f3715763c06c3d682e46ddfd2b6e4ffa59335ebc2e9b.ts/SHA256E-s648976--241238c948aad3df0871f3715763c06c3d682e46ddfd2b6e4ffa59335ebc2e9b.ts
