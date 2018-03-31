f892a1845b62bcda1cddfd5c3054d6de677ced3e
Thibault Saunier <tsaunier@igalia.com>
2018-03-31 13:18:29 -0300

    Fix previous commit
    And do not mention git-annex anymore

--- testsuites/testsuiteutils.py
+++ testsuites/testsuiteutils.py
@@ -107,13 +107,12 @@ def update_assets(options, assets_dir):
         CHECKOUT_BRANCH_COMMAND = "git fetch origin && (git checkout origin/%s || git checkout tags/%s)" % (
             GST_VALIDATE_TESTSUITE_VERSION, GST_VALIDATE_TESTSUITE_VERSION)
         if options.force_sync:
-            subprocess.check_call(["git", "reset", "--hard"], cwd=asset_dir)
+            subprocess.check_call(["git", "reset", "--hard"], cwd=assets_dir)
         print("Checking out %s" % GST_VALIDATE_TESTSUITE_VERSION)
         subprocess.check_call(CHECKOUT_BRANCH_COMMAND, shell=True, cwd=assets_dir)
         download_files(os.path.basename(os.path.join(assets_dir)))
     except Exception as e:
-        print("\nERROR: Could not update assets \n\n%s"
-              "\n\nMAKE SURE YOU HAVE git-annex INSTALLED!" % (e))
+        print("\nERROR: Could not update assets \n\n: %s" % e)
 
         return False
 
