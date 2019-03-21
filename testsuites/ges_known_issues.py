c85ec3ba1b5f58e757e2b57f10845ef591aae835
Thibault Saunier <tsaunier@igalia.com>
2019-03-21 10:00:42 -0300

    ges:known_issues: Document gstreamer/gst-editing-services#65

--- testsuites/ges_known_issues.py
+++ testsuites/ges_known_issues.py
@@ -8,4 +8,17 @@ KNOWN_ISSUES = {
             }
         ]
     },
+    "https://gitlab.freedesktop.org/gstreamer/gst-editing-services/issues/65": {
+        "tests": [
+            "ges.playback.scrub_backward_seeking.test_transition.*"
+        ],
+        "issues": [
+            {
+                "issue-id": "event::segment-has-wrong-start",
+                "summary": "A segment doesn't have the proper time value after an ACCURATE seek",
+                "level": "critical",
+                "can-happen-several-times": True,
+            },
+        ],
+    },
 }
