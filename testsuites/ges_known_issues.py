1060503e779253b46ce7c15752d2e45abbc6c958
Thibault Saunier <tsaunier@igalia.com>
2019-03-25 17:23:26 -0300

    known-issues: Bug gstreamer/gst-editing-services#27 also happenes on add_effect_and_split

--- testsuites/ges_known_issues.py
+++ testsuites/ges_known_issues.py
@@ -1,11 +1,18 @@
 KNOWN_ISSUES = {
     'https://gitlab.freedesktop.org/gstreamer/gst-editing-services/issues/27': {
-        'tests': ['ges.playback.scrub_forward_seeking.test_mixing.*mp3.*'],
+        'tests': [
+            'ges.playback.scrub_forward_seeking.test_mixing.*mp3.*',
+            'ges.scenario.add_effect_and_split',
+        ],
         'issues': [
             {
                 'summary': 'position after a seek is wrong',
                 'sometimes': True,
-            }
+            },
+            {
+                "summary": "The execution of an action did not properly happen",
+                "issue-id": "scenario::execution-error",
+            },
         ]
     },
     "https://gitlab.freedesktop.org/gstreamer/gst-editing-services/issues/65": {
